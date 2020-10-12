2730a9bb21f650bd67e283d64594c54728ea9aa6
unknown <taveras574@gmail.com>
2020-10-11 22:43:12 -0400

    ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,15 +3,27 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    length = len(arrays)
+    cache = dict()
+    
+    for item in arrays[0]:
+        cache[item] = 1
 
-    return result
+
+    for i in range(1, length):
+        for item in arrays[i]:
+            if item in cache:
+                cache[item] += 1
+
+    return [k for k,v in cache.items() if v is length]
+    
 
 
-if __name__ == "__main__":
-    arrays = []
+# if __name__ == "__main__":
+#     arrays = []
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+#     arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+#     arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+#     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
-    print(intersection(arrays))
+#     print(intersection(arrays))
