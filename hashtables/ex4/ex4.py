93f6edddb66ee592df19498987c64f571e53acc8
unknown <taveras574@gmail.com>
2020-10-11 23:00:55 -0400

    Ext 4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,9 +3,20 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    result = []
+    for i in a:
+        if abs(i) in cache:
+            result.append(abs(i))
+        cache[abs(i)] = i
 
     return result
 
+    
 
-if __name__ == "__main__":
-    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+
+
+print(has_negatives([ 1, -1, 2, 3, -4, -3, 4, -5, 6, 7 ]))
+
+# if __name__ == "__main__":
+#     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
