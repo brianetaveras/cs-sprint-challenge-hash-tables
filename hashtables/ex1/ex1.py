021f52d91eccd64e83468ed38caf56c77621e63f
unknown <taveras574@gmail.com>
2020-10-11 21:01:22 -0400

    finished ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,4 +4,16 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
 
+    hash_table = {}
+
+    for i in range(len(weights)):
+
+        target = limit - weights[i]
+        if target in hash_table:
+           return (i, hash_table[target])
+
+        hash_table[weights[i]] = i
+
     return None
+
+
