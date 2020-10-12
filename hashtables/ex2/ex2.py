15682eb4a711759333d34cef5fb645aa0d3fd83f
unknown <taveras574@gmail.com>
2020-10-11 21:53:48 -0400

    Ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,31 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    route = []
+    table = {}
 
+    for ticket in tickets:
+        table[ticket.source] = ticket.destination
+    
+    current = "NONE"
+    while table[current] != "NONE":
+        route.append(table[current])
+        current = table[current]
+    route.append(table[current])
     return route
+
+
+tickets = [
+    Ticket("PIT", "ORD" ),
+    Ticket("XNA", "CID" ),
+    Ticket("SFO", "BHM" ),
+    Ticket("FLG", "XNA" ),
+    Ticket("NONE", "LAX"),
+    Ticket("LAX", "SFO" ),
+    Ticket("CID", "SLC" ),
+    Ticket("ORD", "NONE"),
+    Ticket("SLC", "PIT" ),
+    Ticket("BHM", "FLG" )
+]
+
+print(reconstruct_trip(tickets, 10))
